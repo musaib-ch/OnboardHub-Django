78360dfef8eb6e48e1fec4dbf03e6eb665d0ffbe
musaib-ch <164421240+musaib-ch@users.noreply.github.com>
2026-09-11 16:06:41 +0500

    Expose Gmail OAuth settings routes

--- onboardhub/urls.py
+++ onboardhub/urls.py
@@ -6,6 +6,7 @@ from django.urls import include, path
 urlpatterns = [
     path("django-admin/", admin.site.urls),
     path("", include("core.urls")),
+    path("", include("core.gmail_urls")),
 ]
 
 if settings.DEBUG:
